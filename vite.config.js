a73179929b6b1ae5adbe0fb5afa9385c42a88eb1
WebHead-20 <headweb89@gmail.com>
2023-07-13 23:53:26 +0530

    Laravel Movie App

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import purge from '@erbelion/vite-plugin-laravel-purgecss'
 
 export default defineConfig({
     plugins: [
@@ -8,8 +7,5 @@ export default defineConfig({
             input: ['resources/css/app.css'],
             refresh: true,
         }),
-        purge({
-            templates: ['blade']
-        })
     ],
 });
