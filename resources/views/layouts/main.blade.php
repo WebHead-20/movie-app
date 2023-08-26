bd03322880753a0c47b6143abe2c2fe88f2ddbf8
Chirantan Oke <102691933+WebHead-20@users.noreply.github.com>
2023-08-26 14:54:55 +0530

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <link rel="icon" type="image/x-icon" href="./favicon.ico">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" />
+
     @vite(['resources/css/app.css'])
     @livewireStyles
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
